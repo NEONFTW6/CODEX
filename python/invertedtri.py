65e9b7fa15a14865849f118531dfcc64ab58a800
Nirav Niyaran <niravniyaran89@gmail.com>
2025-11-28 21:08:57 +0530

    updated files

--- python/invertedtri.py
+++ python/invertedtri.py
@@ -1,6 +1,8 @@
 n = int(input("Enter your value: "))
-
-for i in range(n):
-    for j in range(i+1):
+i=n
+for n in range(i):
+    for j in range(i-((n+1)-1)):
         print("*",end=" ")
     print()
+
+
